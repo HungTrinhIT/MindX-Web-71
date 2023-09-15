5c32af33148e310eaa85bc4edcf0b5242be8a26d
HungTrinhIT <minhhung.it.work@gmail.com>
2023-09-15 19:09:37 +0700

    update

---
+++ lesson04-mongodb/products.js
@@ -0,0 +1,32 @@
+[
+  {
+    item: 'journal',
+    quantity: 25,
+    size: { h: 14, w: 21, uom: 'cm' },
+    status: 'A',
+  },
+  {
+    item: 'notebook',
+    quantity: 10,
+    size: { h: 8.5, w: 11, uom: 'in' },
+    status: 'A',
+  },
+  {
+    item: 'paper',
+    quantity: 100,
+    size: { h: 8.5, w: 11, uom: 'in' },
+    status: 'D',
+  },
+  {
+    item: 'planner',
+    quantity: 75,
+    size: { h: 22.85, w: 30, uom: 'cm' },
+    status: 'D',
+  },
+  {
+    item: 'postcard',
+    quantity: 45,
+    size: { h: 10, w: 15.25, uom: 'cm' },
+    status: 'A',
+  },
+];
