f2d20908a7e7fe5d2f70271aa8e2d4e7d3e2fdce
tranminhwang <j2team.tranminhquang@gmail.com>
2023-09-25 20:53:07 +0700

    hate you

--- social-app-server/server.js
+++ social-app-server/server.js
@@ -6,7 +6,7 @@ import { connectToDatabase } from './config/database.js';
 
 import cors from 'cors';
 
-const whitelist = ['https://social-app-server-p5cm.onrender.com'];
+const whitelist = ['https://social-app-server-p5cm.onrender.com', 'https://social-app-client-a2fz.onrender.com', 'http://localhost:8081'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (process.env.NODE_ENV === 'development') {
