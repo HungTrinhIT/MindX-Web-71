b315ab8b791cf621487bc1d72d2447d4a167d900
tranminhwang <j2team.tranminhquang@gmail.com>
2023-09-25 21:16:32 +0700

    fix cors

--- social-app-server/server.js
+++ social-app-server/server.js
@@ -3,19 +3,20 @@ import express from 'express';
 import appRouter from './routes/index.js';
 import apiLoggerMiddleware from './middlewares/apiLogger.mdw.js';
 import { connectToDatabase } from './config/database.js';
+import handleErrorMiddleware from './middlewares/handleError.js';
 
 import cors from 'cors';
 
 const whitelist = ['https://social-app-server-p5cm.onrender.com', 'https://social-app-client-a2fz.onrender.com', 'http://localhost:8081'];
 const corsOptions = {
   origin: function (origin, callback) {
-    if (process.env.NODE_ENV === 'development') {
-      return callback(null, true);
-    }
+    // if (process.env.NODE_ENV === 'development') {
+    //   return callback(null, true);
+    // }
     if (whitelist.includes(origin)) {
       callback(null, true);
     } else {
-    //  callback(new Error('Not allowed by CORS'), false);
+     callback(new Error('Not allowed by CORS'));
     }
   },
   methods: 'PUT,PATCH,GET,DELETE,UPDATE',
@@ -31,6 +32,7 @@ connectToDatabase()
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(apiLoggerMiddleware);
+app.use(handleErrorMiddleware);
 
 // 3. Define routes
 app.use('/api/v1', appRouter);
