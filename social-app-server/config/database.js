f8a92e5871cb07fd48e3b41116fdfe7caf66280e
HungTrinhIT <minhhung.it.work@gmail.com>
2023-09-29 18:04:54 +0700

    feat: update DB host

--- social-app-server/config/database.js
+++ social-app-server/config/database.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const MONGO_URI = 'mongodb://localhost:27017';
+const MONGO_URI = process.env.MONGO_URI;
 const DATABASE = 'social-apps-web-71';
 
 const db = {};
