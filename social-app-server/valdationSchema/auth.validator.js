26cc2d65b8734e43ea29bc4a840c5e8db9a8805e
HungTrinhIT <minhhung.it.work@gmail.com>
2023-09-29 21:57:42 +0700

    test

--- social-app-server/valdationSchema/auth.validator.js
+++ social-app-server/valdationSchema/auth.validator.js
@@ -2,7 +2,13 @@ import * as yup from 'yup';
 
 const registerSchema = yup.object().shape({
   email: yup.string().email().required(),
-  password: yup.string().min(6).required(),
+  password: yup
+    .string()
+    .matches(
+      /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/,
+      'Password must contain at least one number and one special character'
+    )
+    .min(8, 'Password must be at least 8 characters long'),
   fullname: yup.string().required(),
   phoneNumber: yup.string().optional(),
   gender: yup.string().oneOf(['male', 'female', 'other']).required(),
